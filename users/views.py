6cf1a2e34a7c645cb2f79d8027258d2c60a4892c
김병훈 <figma@kakao.com>
2021-05-08 21:33:29 +0900

    #18 Login with Kakao

--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import authenticate, get_user_model
 from django.contrib.auth.decorators import login_required
+from django.core.files.base import ContentFile
 from .forms import LoginForm, CustomUserCreationForm
 
 
@@ -120,7 +121,7 @@ def get_token_from_github(code):
             "Accept": "application/json",
         },
     )
-    return token_request
+    return token_request.json()
 
 
 def get_user_from_github(token_json):
@@ -138,15 +139,14 @@ def github_callback(request):
         code = request.GET.get("code", None)
         if code is None:
             raise GithubException()
-        token_request = get_token_from_github(code)
-        token_json = token_request.json()
+        token_json = get_token_from_github(code)
         # 에러가 담겨 오는 경우
         if token_json.get("error", None):
             raise GithubException()
         # 토큰 요청 완료
 
         # 토큰을 이용하여, 유저 정보 가져오기
-        user_request = get_user_from_github()
+        user_request = get_user_from_github(token_json)
 
         # status code를 확인하여, 성공적으로 수행되었는지 확인합니다.
         if user_request.status_code != 200:
@@ -169,6 +169,7 @@ def github_callback(request):
             user = UserModel.objects.get(email=email)
             # 깃허브 로그인이 아닌 유저라면, 에러 발생
             if user.login_method != UserModel.LOGIN_GITHUB:
+                print("동일한 이메일, 다른 로그인 방식")
                 raise GithubException
         except UserModel.DoesNotExist:
             # 이용자 생성
@@ -178,6 +179,7 @@ def github_callback(request):
                 bio=bio,
                 email=email,
                 login_method=UserModel.LOGIN_GITHUB,
+                email_verified=True,
             )
             # 임의의 비밀번호 생성
             user.set_unusable_password()
@@ -187,4 +189,92 @@ def github_callback(request):
         return redirect("core:index")
     except GithubException:
         # to do: 에러 메세지
+        return redirect("users:login")
+
+
+# 카카오 로그인
+def kakao_login(request):
+    REST_API_KEY = os.environ.get("KAKAO_KEY")
+    REDIRECT_URI = "http://127.0.0.1:8000/users/login/kakao/callback"
+    REQUIRED_SCOPES = "profile,account_email,gender"
+    REQUEST_URL = f"https://kauth.kakao.com/oauth/authorize?client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&response_type=code&scope={REQUIRED_SCOPES}"
+    return redirect(REQUEST_URL)
+
+
+class KakaoException(Exception):
+    pass
+
+
+def get_kakao_token(code):
+    TOKEN_URL = "https://kauth.kakao.com/oauth/token?"
+    parameters = {
+        "grant_type": "authorization_code",
+        "client_id": os.environ.get("KAKAO_KEY"),
+        "redirect_uri": "http://127.0.0.1:8000/users/login/kakao/callback",
+        "code": code,
+    }
+    for key, value in parameters.items():
+        TOKEN_URL += f"{key}={value}&"
+    response = requests.post(TOKEN_URL)
+    return response.json()
+
+
+def get_kakao_user_data(access_token):
+    USER_URL = "https://kapi.kakao.com/v2/user/me"
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+    }
+    response = requests.get(USER_URL, headers=headers)
+    return response.json()
+
+
+def kakao_callback(request):
+    # Error가 발생한 경우
+    try:
+        if request.GET.get("error", None):
+            raise KakaoException
+        code = request.GET.get("code")
+        token_json = get_kakao_token(code)
+        access_token = token_json.get("access_token")
+        error = token_json.get("error")
+        if error:
+            raise KakaoException
+
+        user_json = get_kakao_user_data(access_token)
+        # 이름, 이메일, 프로필 이미지
+        name = user_json.get("kakao_account").get("profile").get("nickname")
+        email = user_json.get("kakao_account").get("email")
+        profile_image_url = (
+            user_json.get("kakao_account").get("profile").get("profile_image_url")
+        )
+        # 동일한 이메일 확인
+        try:
+            user = UserModel.objects.get(username=email)
+            # 동일한 유저가 존재함
+            if user.login_method == "kakao":
+                # 로그인하기
+                auth_login(request, user)
+                return redirect("core:index")
+            print("동일한 이메일, 다른 로그인 방식")
+            raise KakaoException
+        except UserModel.DoesNotExist:
+            user = UserModel.objects.create(
+                username=email,
+                first_name=name,
+                email=email,
+                login_method=UserModel.LOGIN_KAKAO,
+                email_verified=True,
+            )
+            user.set_unusable_password()
+            user.save()
+            # 사진이 있는지 확인
+            if profile_image_url:
+                photo_request = requests.get(profile_image_url)
+                user.avatar.save(
+                    f"{name}-avatar.jpg", ContentFile(photo_request.content)
+                )
+            # 로그인
+            auth_login(request, user)
+            return redirect("core:index")
+    except KakaoException:
         return redirect("users:login")
